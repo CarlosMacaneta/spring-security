397a15bde99be79018774c50ab8d25851a2e8137
Carlos Macaneta <carlosmacaneta@hotmail.com>
2021-09-01 22:19:12 +0200

    Basic auth - Creating in memory user with password encode BCrypt

---
+++ src/main/java/com/security/demo/security/PasswordConfig.java
@@ -0,0 +1,19 @@
+package com.security.demo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordConfig {
+    
+    /**
+     * Encodes password - BCrypt
+     * @return encoded password
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+}
