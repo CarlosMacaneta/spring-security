699924f6a8c8765778d5fd2db860c5a6f3ee07cc
Carlos Macaneta <carlosmacaneta@hotmail.com>
2021-09-03 18:44:41 +0200

    Permissions based authentication

--- src/main/java/com/security/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/security/demo/security/ApplicationSecurityConfig.java
@@ -3,6 +3,8 @@ package com.security.demo.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,6 +16,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true) //enables pre-authorize annotations
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
@@ -31,12 +34,18 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
      * Basic Auth
      * 
      * using antMatchers to wide list permission withou auth
+     * Those comment lines were replaced by annotations 
      */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.csrf().disable()
+            .authorizeRequests()
             .antMatchers("/", "index", "/css/", "/js/*").permitAll()
             .antMatchers("/api/**").hasRole(Role.STUDENT.name())
+            /*.antMatchers(HttpMethod.DELETE, "management/api/**").hasAuthority(UserPermission.COURSE_WRITE.getPermission())
+            .antMatchers(HttpMethod.POST, "management/api/**").hasAuthority(UserPermission.COURSE_WRITE.getPermission())
+            .antMatchers(HttpMethod.PUT, "management/api/**").hasAuthority(UserPermission.COURSE_WRITE.getPermission())
+            .antMatchers(HttpMethod.GET, "management/api/**").hasAnyRole(Role.ADMIN.name(), Role.ADMIN_TRAINEE.name())*/
             .anyRequest()
             .authenticated()
             .and()
@@ -53,16 +62,25 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails user = User.builder()
             .username("annasmith")
             .password(passwordEncoder.encode("password"))
-            .roles(Role.STUDENT.name())
+            //.roles(Role.STUDENT.name())
+            .authorities(Role.STUDENT.getGrantedAuthority())
             .build();
         
         UserDetails admin =User.builder()
             .username("linda")
             .password(passwordEncoder.encode("password123"))
-            .roles(Role.ADMIN.name())
+            //.roles(Role.ADMIN.name())
+            .authorities(Role.ADMIN.getGrantedAuthority())
             .build();
 
-        return new InMemoryUserDetailsManager(admin, user);
+        UserDetails tomUser = User.builder()
+            .username("tom")
+            .password(passwordEncoder.encode("password1"))
+            //.roles(Role.ADMIN_TRAINEE.name())
+            .authorities(Role.ADMIN_TRAINEE.getGrantedAuthority())
+            .build();
+
+        return new InMemoryUserDetailsManager(admin, user, tomUser);
     }
     
 }
--- src/main/java/com/security/demo/security/Role.java
+++ src/main/java/com/security/demo/security/Role.java
@@ -1,13 +1,18 @@
 package com.security.demo.security;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import com.google.common.collect.Sets;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
 import static com.security.demo.security.UserPermission.*;
 
 public enum Role {
     STUDENT(Sets.newHashSet()),
-    ADMIN(Sets.newHashSet(COURSE_READ, COURSE_WRITE, STUDENT_READ, STUDENT_WRITE));
+    ADMIN(Sets.newHashSet(COURSE_READ, COURSE_WRITE, STUDENT_READ, STUDENT_WRITE)),
+    ADMIN_TRAINEE(Sets.newHashSet(COURSE_READ, STUDENT_READ));
 
     private final Set<UserPermission> permissions;
 
@@ -25,4 +30,13 @@ public enum Role {
         return permissions;
     }
     
+    public Set<SimpleGrantedAuthority> getGrantedAuthority() {
+        Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+            .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+            .collect(Collectors.toSet());
+        
+        permissions.add(new SimpleGrantedAuthority("ROLE_"+this.name()));
+
+        return permissions;
+    }
 }
